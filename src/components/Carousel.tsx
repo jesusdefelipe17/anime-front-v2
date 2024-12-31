ca8a81fc56fc1ea1cc862549aeb3a4e91a19aeaf
kepchu11 <kepchu11@gmail.com>
2024-12-31 14:15:37 +0100

    fix de 2 en 2 los carrouseles

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -21,15 +21,15 @@ const Carousel: React.FC<CarouselProps> = ({ manwhas, title, type }) => {
     responsive: [
       {
         breakpoint: 1024,
-        settings: { slidesToShow: 6, slidesToScroll: 1 },
+        settings: { slidesToShow: 6, slidesToScroll: 6 },
       },
       {
         breakpoint: 768,
-        settings: { slidesToShow: 4, slidesToScroll: 1 },
+        settings: { slidesToShow: 4, slidesToScroll: 4 },
       },
       {
         breakpoint: 480,
-        settings: { slidesToShow: 2, slidesToScroll: 1 },
+        settings: { slidesToShow: 2, slidesToScroll: 2 },
       },
     ],
   };
